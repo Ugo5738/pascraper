a8bce8ab020ec7195285812fcfd06c8deb79deeb
daniel <danielnwachukwu5738@gmail.com>
2025-02-27 12:11:35 +0100

    Update: added signals to app config

--- sitescrapers/apps.py
+++ sitescrapers/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class SitescrapersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sitescrapers"
+
+    def ready(self):
+        # Import the signals module to ensure signal handlers are registered.
+        import sitescrapers.signals
+
+        print("Sitescrapers signals have been imported and registered.")
